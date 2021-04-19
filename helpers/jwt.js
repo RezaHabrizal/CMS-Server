2dceeb5b4123ea92f7493cfc5538e255f2b1e375
RezaHabrizal <jreza.habrizal@gmail.com>
2021-04-19 07:42:29 +0700

    update

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const SECRET_JWT = proccess.ENV.SECRET_JWT
+const SECRET_JWT = 'rahasia'
 
 function signJwt(payload) {
     return jwt.sign(payload, SECRET_JWT)
